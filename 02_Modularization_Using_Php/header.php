6484636511abe369d85c846bdae1647eb58a45e9
derek ney <deek311@gmail.com>
2023-03-27 18:20:14 -0600

    php added

---
+++ 02_Modularization_Using_Php/header.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="HTML TEMP">
+<meta name="author" content="Derek Ney">
+<title>HTML TEMP</title>
+  
+<!-- TELLS PHONES NOT TO LIE ABOUT THEIR WIDTH & stops the font from enlarging when a phone is turned sideways-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <!-- STYLE SHEETS -->
+<link href="css/normalize.css" rel="stylesheet">
+<!-- phone-default -->
+<link href="css/small.css" rel="stylesheet">
+<!-- enhance-tablet -->
+<link href="css/medium.css" rel="stylesheet">
+<!-- enhance-desktop -->
+<link href="css/large.css" rel="stylesheet">
+<!-- phone-mobile -->
+<link href="css/mobile.css" rel="stylesheet">
+<!-- reset -->
+<link href="css/reset.css" rel="stylesheet">
+
+
+  
+   
+</head>
+
+
+    <!-- header -->
+
+	<header class="clearfix">
+    
+        <figure class="logo">
+            <img src="img/logo1.png" alt="logo" srcset="img/logo2.png 1x, img/logo3.png 2x, img/logo4.png 3x">
+</figure>
+<div class="number">
+    <h2>1-800-555-1234</h2>
+</div>
+</header>
+
+<!-- navigation -->
+<nav class="clearfix">
+    <div class="container">
+    <button id="hamburgerBtn">
+        <span>&#9776;</span>
+        <span>&#9776;</span>
+    </button>
+    
+    <ul id="primaryNav">
+        <div class="align">
+        <li class="active"><a href="#">HOME</a></li>
+        <li class="partent"><a href="#">AGENTS &#8595</a>
+            <ul>
+                <li><a herf="#">NEW</a></li>
+                <li><a herf="#">QUESTIONS</a></li>
+            </ul>
+            <li><a href="#">CRUISES</a></li>
+        <li><a href="#">BOOK</a></li>
+    </div>
+ 
+    <div class="search">
+    <form action="#">
+        <button type="submit"><i class="fa fa-search"></i>
+        <input type="text" placeholder="Search" name="search">
+    </button>
+       
+    </form> 
+</div> 
+</ul>
+
+</div>
+</nav>
+    
+
+
+<body>
+    <!-- swiper images -->
---
+++ 02_Modularization_Using_Php/index.php
@@ -0,0 +1,97 @@
+<?php include 'header.php';  ?>
+    <main> 
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+            <picture>
+                <source srcset="img/slider1_small.png" media="(max-width: 600px)">
+                <source srcset="img/slider1_medium.png" media="(max-width: 900px)">
+        <img src="img/slider1_large.png" alt="slider">
+  
+        <p><b>Test image1</b> - this is just test images to see how sliders work</p>
+    </picture> 
+     </div>
+
+    <div class="swiper-slide">
+        <picture>
+            <source srcset="img/slider2_small.png" media="(max-width: 600px)">
+            <source srcset="img/slider2_medium.png" media="(max-width: 900px)">
+    <img src="img/slider2_large.png" alt="slider">
+
+    <p><b>Test image2</b> - this is just test images to see how sliders work</p>
+</picture> 
+</div>
+
+
+<div class="swiper-slide">
+    <picture>
+        <source srcset="img/slider3_small.png" media="(max-width:600px)">
+        <source srcset="img/slider3_medium.png" media="(max-width: 900px)">
+<img src="img/slider3_large.png" alt="slider">
+
+<p><b>Test image3</b> - this is just test images to see how sliders work</p>
+</picture> 
+</div>
+    </div>
+ <div class="swiper-pagination">
+
+ </div>
+</div>
+
+       <!-- blog -->
+
+ 
+        <section id= "blogGallery">
+            <h1>Recent Blog Articles</h1>
+            <div class = "flexed">
+
+            <div class="column small-12 medium-6 large-3">
+                <div class="card">
+            <img src="img/img1.png" alt="get away">
+                <p>How do i get away today?</p>
+        
+        </div>
+        <div class="bottomCard">
+            <a>2/23/23</a>
+        </div>
+    </div>
+
+    <div class="column small-12 medium-6 large-3">
+        <div class="card">
+              <img src="img/img2.png" alt="plan trip">
+              <p>Where can I plan my next trip?</p>
+           
+            </div>
+            <div class="bottomCard">
+                <a>2/23/23</a>
+              </div>
+        </div>
+    
+    
+        <div class="column small-12 medium-6 large-3">
+        <div class="card">
+              <img src="img/img3.png" alt="visit places">
+              <p>best places to visit</p>
+         
+            </div>
+            <div class="bottomCard">
+                <a>2/23/23</a>
+              </div>
+        </div>
+
+       
+            <div class="column small-12 medium-6 large-3">
+                <div class="card">
+              <img src="img/img4.png" alt="need for trip">
+              <p>Things you might need on your trip</p>
+            </div>
+            <div class="bottomCard">
+                <a>2/23/23</a>
+                </div>
+        </div>
+      
+</div>
+    </section>
+    </main>
+   <!-- footer -->
+   <?php include 'footer.php';  ?>
